d9e025a03ccf4878ebed43d23538faef52103655
victoronyonkaoroo <victor.oroo@student.moringaschool.com>
2023-07-26 23:49:21 +0300

    done lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
+import { server } from './mocks/server';
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion, onUpdateCorrectAnswer }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  const handleCorrectAnswerChange = (event) => {
+    const updatedCorrectIndex = parseInt(event.target.value);
+    server
+      .patch(`http://localhost:4000/questions/${id}`, { correctIndex: updatedCorrectIndex })
+      .then((res) => {
+        onUpdateCorrectAnswer(res.data);
+      })
+      .catch((error) => {
+        console.error('Error updating the correct answer:', error);
+      });
+  };
+
+  const handleDelete = () => {
+    server
+      .delete(`http://localhost:4000/questions/${id}`)
+      .then(() => {
+        onDeleteQuestion(id);
+      })
+      .catch((error) => {
+        console.error('Error deleting the question:', error);
+      });
+  };
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +39,11 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleCorrectAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
